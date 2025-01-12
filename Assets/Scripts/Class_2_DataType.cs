c89ca8841a040ec4912730b61a9a6ccd8916e869
wannalazy <ws501234580@gmail.com>
2025-01-13 01:08:28 +0800

    Update 認識資料類型

--- Assets/Scripts/Class_2_DataType.cs
+++ Assets/Scripts/Class_2_DataType.cs
@@ -20,4 +20,19 @@ public class Class_2_Da : MonoBehaviour
     public string charactor = "蓋倫";
     public bool isDead = false;      // 是否死亡 : 否 
     public bool gameover = true;     // 是否結束 : 是
+
+    // 整數資料類型
+    public byte level = 16;          // byte 型別，範圍 0 ~ 255 ， 大小 8 byte
+                                     // 等級變數最多就16等 不需要用太大的型別去存取 例如 : int 範圍大 會吃記憶體
+    public uint coin = 9999;         // uint 型別，範圍 0 ~ 4XXX，大小 32 byte
+    public long items = 3000;        // long 型別，範圍 0 ~ 92XXX，大小 64 byte
+    // 以上整數型別還有更多詳見 Microsoft C#資料類型 官網
+
+    // 字串與字元
+    public string playerName = "WRX";
+    public char a = 'a'; //僅能存放單個字元
+
+    //溢位(超出範圍)
+    // public sbyte number = 128;     // 溢位導致錯誤，會出現紅色波浪底線
+
 }
