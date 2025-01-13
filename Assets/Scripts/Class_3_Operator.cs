96b71c3508fae258b3a23650d7f65ab11013a8bb
wannalazy <ws501234580@gmail.com>
2025-01-13 21:30:08 +0800

    Update 認識程式運算子

--- Assets/Scripts/Class_3_Operator.cs
+++ Assets/Scripts/Class_3_Operator.cs
@@ -6,6 +6,7 @@
 /// </summary>
 public class Class_3_Operator : MonoBehaviour
 {
+    #region 基本的欄位屬性
     // C# 變數 = 欄位 Field
     // 欄位屬性 Field Attrite
     // [標題(標題文字)]
@@ -23,4 +24,77 @@ public class Class_3_Operator : MonoBehaviour
     // [文字範圍(最小行，最大行)] : 使用在字串上，設定面板的文字框範圍
     [TextArea(2, 5)]
     public string itemDescription = "這裡是道具說明，這裡是道具說明，這裡是道具說明，";
+
+    // 多個欄位屬性
+    [Header("血量")][Range(0, 999)]
+    public int hp = 100;
+    [Header("攻擊力"), Range(0, 100)]
+    public float attack = 30.5f;
+    // [在屬性面板隱藏] : 將公開變數隱藏
+    [HideInInspector]
+    public string password = "我是密碼";
+    // [序列化欄位] : 將私人變數顯示
+    [SerializeField]
+    private float mp = 500;
+    #endregion
+
+    // 使用 Unity 的事件
+    // 1. 必須在腳本後面添加 : MonoBehaviour (繼承)
+    // 2. 使用關鍵字快速完成，例如: 輸入 Awake 挑選要使用的事件按 Enter
+    // 喚醒事件 : 撥放遊戲後會執行第一個事件，只執行一次
+    // 事件名稱是藍色的 (沒有變色不影響)
+    private void Awake()
+    {
+        #region 輸出訊息
+        // 將小括號內的訊息輸出到 Uniyt 的 Console 控制台面板
+        // Ctrl + Shift + C 開啟控制台面板
+        Debug.Log("哈囉，沃德 :D");       // 輸出文字
+        Debug.Log(hp);                   // 輸出變數的值
+        Debug.Log("攻擊力 : " + attack);  // 字串與變數
+        Debug.Log($"攻擊力 : {attack}");  // $字串格式
+        Debug.Log("攻擊力 : {attack}");   // 沒有寫$
+
+        Debug.Log(("<b>粗子體</b>"));
+        Debug.Log("<color=orange>橘色</color>");
+        // 色碼 :＃紅紅綠綠藍，數值 : 0 ~ 9，a ~ f 越右邊顏色月亮 #000 黑 #fff 白
+        Debug.Log("<color=#66aaff>藍色</color>");
+        Debug.Log("<color=#6af>藍色</color>");
+        #endregion
+
+        #region 算術運算子
+        Debug.Log("<color=#f93>--- 算數運算子 ---</color>");
+        // 運算子 : 
+        // 1. 算數運算子
+        // 加、檢、乘、除、餘
+        // + - * / %
+        Debug.Log(10 + 3);       // 13
+        Debug.Log(10 - 3);       // 7
+        Debug.Log(10 * 3);       // 30
+        Debug.Log(10 / 3);       // 3
+        Debug.Log(10 % 3);       // 10/3=3餘1，結果 1 
+
+
+        Debug.Log("<color=#f93>--- 算數運算子 : 區域變數 ---</color>");
+        // 區域變數 : 僅在此打括號內存取 (不需要修飾詞的變數)
+        float numberA = 10;
+        float numberB = 3;
+
+        Debug.Log(numberA + numberB);
+        Debug.Log(numberA - numberB);
+        Debug.Log(numberA * numberB);
+        Debug.Log(numberA / numberB);    // 3.333
+        Debug.Log(numberA % numberB);
+        #endregion
+
+        Debug.Log("<color=#f93>--- 比較運算子 ---</color>");
+        // 比較運算子，使用後會得到布林值
+        int numberC = 100, numberD = 1;
+
+        Debug.Log(numberC > numberD);    // 大於 True
+        Debug.Log(numberC < numberD);    // 小於 False
+        Debug.Log(numberC >= numberD);   // 大於等於 True 
+        Debug.Log(numberC <= numberD);   // 小於等於 False 
+        Debug.Log(numberC == numberD);   // 等於 False 
+        Debug.Log(numberC != numberD);   // 不等於 True
+    }
 }
