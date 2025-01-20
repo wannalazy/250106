e860b771b73ad7955dac4360abb1027d1a66749f
wannalazy <ws501234580@gmail.com>
2025-01-20 18:49:40 +0800

    Update practice_3_operator.cs

--- Assets/Practice/practice_3_operator.cs
+++ Assets/Practice/practice_3_operator.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 public class practice_3_operator : MonoBehaviour
 {
     private void Awake()
-    { 
-        Debug.Log(6 / 2 * (1 + 2));
+    {
+        // 6 ÷ 2 (1 + 2)
+        // 題目問題
+        // 全部寫符號
+        // 或全部省略
+        // 1 或 9
+        int answer = 6 / 2 * (1 + 2);
+        Debug.Log($"<color=#f33>6 / 2 * (1 + 2) = {answer}<color>");
     }
 }
