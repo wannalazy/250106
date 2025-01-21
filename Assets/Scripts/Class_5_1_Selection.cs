6f55540df3a1b214255fa7c565dab62dde3efdf3
wannalazy <ws501234580@gmail.com>
2025-01-21 11:16:05 +0800

    Update 認識switch與列舉

--- Assets/Scripts/Class_5_1_Selection.cs
+++ Assets/Scripts/Class_5_1_Selection.cs
@@ -16,8 +16,16 @@ namespace WRX
     {
         [SerializeField, Header("是否移動")]
         private bool isMove;
-        [SerializeField, Header("通關門檻"),Range(0,50000)]
+        [SerializeField, Header("通關門檻"), Range(0, 50000)]
         private int score = 50000;
+
+        [SerializeField, Header("武器")]
+        private string weapon;
+
+        [SerializeField, Header("血量"),Range(1,100)]
+        private int hp = 100;
+
+
 
         private void Awake()
         {
@@ -38,6 +46,10 @@ namespace WRX
         // 更新事件 : 一秒鐘執行約 60 次 (60FPS) Frame Per Second
         private void Update()
         {
+            // 常用快捷鍵
+            // 1. 格式化 (排版) Ctrl + K D
+            // 2. 程式碼片段 Ctrl + K S
+            #region 判斷式 if
             Debug.Log("<color=#79f>60FPS更新中</color>");
 
             // 如果 isMove 布林值等於 True ，就顯示持續移動
@@ -52,14 +64,133 @@ namespace WRX
             }
 
             // 比較運算子、邏輯運算子結果為布林值
+            // if 會在最上方、else 在最下方，中間可以有多個 else if
+            // 如果 分數 >= 30000 就通關
             if (score >= 30000)
             {
                 Debug.Log("<color=#3f3>關卡通關</color>");
             }
+            // 否則 如果 分數 >= 25000 可以重新挑戰
+            if (score >= 25000)
+            {
+                Debug.Log("<color=#f96>可重新挑戰</color>");
+            }
+            // 否則 如果 分數 >= 20000 可以重新挑戰，但要看廣告
+            if (score >= 20000)
+            {
+                Debug.Log("<color=#f9a>看廣告後即可重新挑戰</color>");
+            }
+            // 否則 失敗
             else
             {
                 Debug.Log("<color=#f33>關卡失敗</color>");
             }
+            #endregion
+
+            // switch 判斷式
+            // switch (要判斷的值) { 陳述式 }
+            // 快速完成 : switch + Tab * 2
+            #region 判斷式 switch
+            switch (weapon)
+            {
+                // case 值 :
+                // 當判斷的值等於值時會執行這裡
+                // break; 跳出判斷式
+                // 如果武器等於小刀，攻擊力等於20
+                // 卜拉哈跟蕾吉娜攻擊力一樣
+                case "卜拉哈":
+                case "蕾吉娜":
+                    Debug.Log("<color=#ff3>攻擊力 : 10000</color>");
+                    break;
+                case "歐爾納":
+                    Debug.Log("<color=#ff3>攻擊力 : 15000</color>");
+                    break;
+                case "兀恩雅":
+                    Debug.Log("<color=#ff3>攻擊力 : 25000</color>");
+                    break;
+
+                default:
+                    Debug.Log("<color=#f9a>武器錯誤</color>");
+                    break;
+            }
+            #endregion
+
+            #region 課堂練習判斷式
+            // 題目 :
+            // 如果 血量 >= 10 快死掉了
+            // 如果 血量 >= 40 警告，快喝水
+            // 如果 血量 >= 60 健康狀態有裝況
+            // 如果 血量 >= 80 血量安全
+            // 如果 血量 >= 0 你已經死了
+            // 判斷式都算同一個判斷式，只會執行一個陳述式(結果)，並且由上往下
+            // 所以當第一個陳述式條件達成，就不會往下執行
+
+            // 錯誤寫法
+            if (hp >= 10)
+            {
+                Debug.Log("<color=#f33>快死掉了</color>");
+            }
+            else if (hp >= 40)
+            {
+                Debug.Log("<color=#f33>警告，快喝水</color>");
+            }
+            else if (hp >= 60)
+            {
+                Debug.Log("<color=#f33>健康狀態有裝況</color>");
+            }
+            if (hp >= 80)
+            {
+                Debug.Log("<color=#f33>血量安全</color>");
+            }
+            else if (hp >= 0)
+            {
+                Debug.Log("<color=#f33>你已經死了</color>");
+            }
+
+            // 正確寫法一(由最先接觸條件寫)
+            if (hp >= 80)
+            {
+                Debug.Log("<color=#f33>血量安全</color>");
+            }
+            else if (hp >= 60)
+            {
+                Debug.Log("<color=#f33>健康狀態有裝況</color>");
+            }
+            else if (hp >= 40)
+            {
+                Debug.Log("<color=#f33>警告，快喝水</color>");
+            }
+            else if (hp >= 10)
+            {
+                Debug.Log("<color=#f33>快死掉了</color>");
+            }
+            else if (hp >= 0)
+            {
+                Debug.Log("<color=#f33>你已經死了</color>");
+            }
+
+            // 正確寫法二(加上並且條件)
+            if (hp >= 10 && hp < 40)
+            {
+                Debug.Log("<color=#f33>快死掉了</color>");
+            }
+            else if (hp >= 40 && hp < 60)
+            {
+                Debug.Log("<color=#f33>警告，快喝水</color>");
+            }
+            else if (hp >= 60 && hp < 80)
+            {
+                Debug.Log("<color=#f33>健康狀態有裝況</color>");
+            }
+            if (hp >= 80)
+            {
+                Debug.Log("<color=#f33>血量安全</color>");
+            }
+            else if (hp >= 0)
+            {
+                Debug.Log("<color=#f33>你已經死了</color>");
+            } 
+            #endregion
         }
     }
 }
---
+++ Assets/Scripts/Class_5_2_Enumeration.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace WRX
+{
+    /// <summary>
+    /// 列舉 : enumeration，C# 內關鍵字為簡寫 enum
+    /// </summary>
+    public class Class_5_2_Enumeration : MonoBehaviour
+    {
+        #region 認識列舉基本用法
+        // 列舉 : 定義下拉選單的選項 (通常都是單選)
+        // 語法 :
+        // 修飾詞 關鍵字 enum 列舉名稱 { 列舉選項 }
+        // 定義一個列舉，名稱叫做季節
+        // 列舉都是數值，預設從 0 開始
+        private enum Season
+        {
+            // 定義列舉選項
+            Spring, Summer, Autumn, Winter
+        }
+
+        // 宣告變數，類型為季節列舉，預設為夏天
+        [SerializeField, Header("季節")]
+        private Season season = Season.Summer;
+
+        private void Awake()
+        {
+            // 取得 列舉值 (Get) 抓出值
+            Debug.Log(season);
+            // 取得列舉值的整數值
+            Debug.Log((int)season);
+
+            // 設定 列舉值 (Set) 修改值
+            season = Season.Winter;
+            Debug.Log(season);
+            // 透過數值設定列舉
+            season = (Season)2;
+            Debug.Log(season);
+        }
+        #endregion
+
+        // 定義列舉數值
+        /// <summary>
+        /// 道具
+        /// </summary>
+        private enum Item
+        {
+            None = 0, Coin = 1, Redwater = 10, Bluewater = 10, Chicken = 20
+        }
+
+        [SerializeField, Header("道具")]
+        private Item item = Item.Chicken;
+
+        // 執行順序 : Awake > Start > Update
+        // 開始事件 : 在喚醒事件後執行一次 (初始化)
+        private void Start()
+        {
+            Debug.Log((int)item);
+        }
+    }
+}
+
