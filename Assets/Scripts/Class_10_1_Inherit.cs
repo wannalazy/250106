ccc1f367e1abea8f6192128d87f0d35e499dd1d0
wannalazy <ws501234580@gmail.com>
2025-02-19 19:48:43 +0800

    Update 認識繼承

---
+++ Assets/Scripts/Class_10_1_Inherit.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using WRX.Tools;
+
+namespace WRX
+{ 
+    /// <summary>
+    /// 繼承 Inherit
+    /// </summary>
+    public class Class_10_1_Inherit : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var goblin1 = new Goblin(10, 1);
+            LogSystem.LogWithColor($"哥布林1號的攻擊 : {goblin1.attack}", "#f3d");
+            var Slime1 = new Slime(3, 2);
+            LogSystem.LogWithColor($"史萊姆1號的攻擊 : {Slime1.attack}", "#03d");
+        }
+    }
+
+    public class Goblin
+    {
+        public int attack;          // 公開 : 所有類別可以存取
+        public int defense;
+        private float moveSpeed;    // 私人 : 此類別可以存取
+        protected float hp;         // 保護 : 子類別可以存取
+
+        public Goblin(int _attack, int _defense)
+        {
+            attack = _attack;
+            defense = _defense;
+        }
+    }
+
+    // 繼承讓類別可以繼承另一個類別，擁有該類別的成員
+    // 子類別 : 父類別
+    // C# 僅提供單一繼承 (只能繼承一個類別)
+    public class Slime : Goblin
+    {
+        public Slime(int _attack, int _defense) : base(_attack, _attack)
+        {
+        }
+
+        public void Intlize()
+        {
+            attack = 7;             // 公開 : 可以存取
+            // moveSpeed = 3.5f;    // 私人 : 無法存取
+            hp = 10;                // 保護 : 可以存取
+        }
+    }
+}
+
--- Assets/Scripts/Class_9_2_DataTypeSwitch.cs
+++ Assets/Scripts/Class_9_2_DataTypeSwitch.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using WRX.Tools;
+using System;
 
 namespace WRX 
 {
@@ -51,6 +52,28 @@ namespace WRX
             LogSystem.LogWithColor(byte4, "#f77");
 
             LogSystem.LogWithColor("---------", "#f33");
+        }
+
+        private void Start()
+        {
+            // 將整數轉為字串
+            int count = 99;
+            var intTostring = Convert.ToString(count);
+            LogSystem.LogWithColor(intTostring.GetType(), "#f96");
+            // 將布林值轉為字串
+            bool isOver = false;
+            var boolTostring = Convert.ToString(isOver);
+            LogSystem.LogWithColor(boolTostring.GetType(), "#f96");
+
+            float move = 3.5f;
+            var floatToByte = Convert.ToByte(move);
+            LogSystem.LogWithColor(floatToByte.GetType(), "#f96");
+
+            // true = 1,  false = 0
+            bool isGround = true;
+            var boolToByte = Convert.ToByte(isGround);
+            LogSystem.LogWithColor(boolToByte, "#6f6");
+            LogSystem.LogWithColor(boolToByte.GetType(), "#6f6");
         }
     }
 }
