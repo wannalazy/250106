dea1a1fc015540a7fd175feec3a1c0d9cbb4b892
wannalazy <ws501234580@gmail.com>
2025-02-24 19:06:33 +0800

    Update 作業

---
+++ Assets/Practice/Practice_9_DataType.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using WRX.Tools;
+
+namespace WRX 
+{
+    /// <summary>
+    /// 資料類型轉換
+    /// </summary>
+    public class Practice_9_DataType : MonoBehaviour
+    {
+        private void Awake()
+        {
+            float number = -999.321f;
+            sbyte byteNumber = (sbyte)number;
+            LogSystem.LogWithColor(byteNumber, "#6f6");
+        }
+    }
+}
+
