1fb9c454797df1c2e38139fa6bfd23e018776214
wannalazy <ws501234580@gmail.com>
2025-03-10 22:46:57 +0800

    Update Class_15_Generics.cs

--- Assets/Scripts/Class_15_Generics.cs
+++ Assets/Scripts/Class_15_Generics.cs
@@ -11,6 +11,7 @@ namespace WRX.Class_15
     {
         private void Awake()
         {
+            #region 不使用泛型
             int numberA = 7, numberB = 9;
             LogSystem.LogWithColor($"數字 A 與 B :{numberA}|{numberB}", "#f93");
             SwapNumber(ref numberA, ref numberB);
@@ -24,9 +25,29 @@ namespace WRX.Class_15
             object objA = 3.5f, objB = 7.7f;
             LogSystem.LogWithColor($"物件 A 與 B :{objA}|{objB}", "#f93");
             SwapObject(ref objA, ref objB);
-            LogSystem.LogWithColor($"物件 A 與 B :{objA}|{objB}", "#f93");
+            LogSystem.LogWithColor($"物件 A 與 B :{objA}|{objB}", "#f93"); 
+            #endregion
+
+            bool boolA = true, boolB = false;
+            LogSystem.LogWithColor($"A 與 B :{boolA}|{boolB}", "#9f9");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSystem.LogWithColor($"A 與 B :{boolA}|{boolB}", "#9f9");
+
+            byte byteA = 1, byteB = 9;
+            LogSystem.LogWithColor($"A 與 B :{byteA}|{byteB}", "#9f9");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSystem.LogWithColor($"A 與 B :{byteA}|{byteB}", "#9f9");
+
+            var player1 = new DataPlayer<int>();
+            player1.data = 99;
+            player1.LogData(123);
+
+            var player2 = new DataPlayer<string>();
+            player2.data = "玩家二號";
+            player2.LogData("哈囉");
         }
 
+        #region 方法區域
         /// <summary>
         /// 數字對調
         /// </summary>
@@ -57,6 +78,24 @@ namespace WRX.Class_15
             object temp = a;                   // 將第一個數字放去旁邊(暫存)
             a = b;                          // 將第二個數字放到第一個數字內
             b = temp;                       // 將旁邊的數字放到第二個數字
+        }
+
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        #endregion
+
+        public class DataPlayer<T>
+        { 
+            public T data;
+
+            public void LogData(T data)
+            {
+                LogSystem.LogWithColor(data, "#3ff");
+            }
         }
     }
 }
